d245c08a22df32d0fa8b18cae3876f371d13de27
Mike O'B-W <68826548+menwacodes@users.noreply.github.com>
2023-02-24 17:07:40 -0500

    Dynamic cards with sorting

--- pages/gardening/index.js
+++ pages/gardening/index.js
@@ -1,14 +1,34 @@
+import {useState} from "react";
 import classes from "./GardeningHome.module.scss"
 
 import FoodCard from "@/components/gardening/food/FoodCard";
 
 const GardeningHomePage = ({allFood, count}) => {
+    const [foodItems, setFoodItems] = useState(allFood.sort((a, b) => a.plant > b.plant ? 1 : -1))
+    //food.sort((a, b) => {return a.plant > b.plant ? 1 : -1})
 
-    const foodCards = allFood.map(foodItem => <FoodCard key={foodItem.slug} food={foodItem}/>);
+    const sortFoodItems = sortBy => {
+        let newFoodItems;
+        if(sortBy === "harvest") {
+            newFoodItems = foodItems.slice().sort((a, b)=> a.lifecycle.grow.daysToHarvest > b.lifecycle.grow.daysToHarvest ? 1: -1)
+        } else if(sortBy === "name") {
+            newFoodItems = foodItems.slice().sort((a, b) => a.plant > b.plant ? 1 : -1)
+        } else {
+            newFoodItems = foodItems.slice()
+        }
+        setFoodItems(newFoodItems)
+    }
+
+    const foodCards = foodItems.map(foodItem => <FoodCard key={foodItem.slug} food={foodItem}/>);
+
     return (
-        <div className={classes.food_grid}>
-            {foodCards}
-        </div>
+        <>
+            <button onClick={() => {sortFoodItems("harvest")}}>Sort by Days to Harvest</button>
+            <button onClick={() => {sortFoodItems("name")}}>Sort by Name</button>
+            <div className={classes.food_grid}>
+                {foodCards}
+            </div>
+        </>
     );
 };
 
