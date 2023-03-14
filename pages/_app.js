e4668f818d52616f959fab0df7b46bbe1ccbf596
mike <menwa.codes@gmail.com>
2023-03-14 07:52:19 -0400

    Redux store started

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,22 @@
-import '@/styles/main.scss'
+import Head from "next/head";
+import {Provider} from "react-redux";
+import {store} from "@/store";
 
+import '@/styles/main.scss';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+    return (
+        <>
+            <Provider store={store}>
+                <Head>
+                    <title>Gardening</title>
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+                    <meta name={"description"} content={"Simple site for the gardening I do"}/>
+                    <meta name={"keywords"} content={"herbs, vegetables, fertilizer"}/>
+                    <meta name={"author"} content={"Mike O'Brien-Walker"}/>
+                </Head>
+                <Component {...pageProps} />
+            </Provider>
+        </>
+    );
 }
---
+++ store/reducers/userReducer.js
@@ -0,0 +1,21 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const DEFAULT_STATE = {
+    loading: false,
+    data: {
+        _id: '',
+        email: '',
+        firstName: '',
+        lastName: '',
+        role: 'user'
+    },
+    auth: false
+};
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: DEFAULT_STATE,
+    reducers: {}
+});
+
+export default userSlice.reducer;
