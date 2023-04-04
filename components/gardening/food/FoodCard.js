ba154e3961f97a57bc314742b2fe24c8e516412d
mike <menwa.codes@gmail.com>
2023-04-04 13:15:16 -0400

    Updated image styling

--- components/gardening/food/FoodCard.js
+++ components/gardening/food/FoodCard.js
@@ -3,21 +3,22 @@ import Link from "next/link";
 import Image from "next/image";
 import classes from "./FoodCard.module.scss";
 
-const FoodCard = ({food, showMaintenance}) => {
+const FoodCard = ({ food, showMaintenance }) => {
     return (
-        <Link href={`/gardening/food/${food.slug}`}>
-            <article className={classes.card}>
-                <Image className={classes.card__img} src={`/img/gardening/${food.image}`} width={"1920"} height={"1080"}
-                       alt={""}/>
-                <div className={classes.card__content}>
-                    <p className={classes.card__title}>{food.plant}</p>
-                    <ul className={classes.card__items}>
+        <Link href={ `/gardening/food/${ food.slug }` }>
+            <article className={ classes.card }>
+                <Image className={ classes.card__img } src={ `/img/gardening/${ food.image }` } width={ "1920" }
+                       height={ "1080" }
+                       alt={ "" }/>
+                <div className={ classes.card__content }>
+                    <p className={ classes.card__title }>{ food.plant }</p>
+                    <ul className={ classes.card__items }>
 
                         {/*
                                 PLANTING ITEMS
-                        */}
+                        */ }
 
-                        {!showMaintenance &&
+                        { !showMaintenance &&
                             <>
                                 <li className={ classes.card__item }>
                                     <svg className={ `${ classes.card__icon } ${ classes.rotate90 }` }>
@@ -44,24 +45,24 @@ const FoodCard = ({food, showMaintenance}) => {
                         }
                         {/*
                                 MAINTENANCE ITEMS
-                        */}
+                        */ }
                         { showMaintenance &&
                             <>
-                            <li className={ classes.card__item }>
-                                <svg className={ classes.card__icon }>
-                                    <use href={ "/img/sprite.svg#icon-calendar" }></use>
-                                </svg>
-                                <span
-                                    className={ classes.card__text }>Days to Germination: { food.lifecycle.seedling.daysToGermination }</span>
-                            </li>
-                            <li className={ classes.card__item }>
-                                <svg className={ classes.card__icon }>
-                                    <use href={ "/img/sprite.svg#icon-calendar" }></use>
-                                </svg>
-                                <span
-                                    className={ classes.card__text }>Days to Harvest: { food.lifecycle.grow.daysToHarvest }</span>
-                            </li>
-                        </>}
+                                <li className={ classes.card__item }>
+                                    <svg className={ classes.card__icon }>
+                                        <use href={ "/img/sprite.svg#icon-calendar" }></use>
+                                    </svg>
+                                    <span
+                                        className={ classes.card__text }>Days to Germination: { food.lifecycle.seedling.daysToGermination }</span>
+                                </li>
+                                <li className={ classes.card__item }>
+                                    <svg className={ classes.card__icon }>
+                                        <use href={ "/img/sprite.svg#icon-calendar" }></use>
+                                    </svg>
+                                    <span
+                                        className={ classes.card__text }>Days to Harvest: { food.lifecycle.grow.daysToHarvest }</span>
+                                </li>
+                            </> }
                     </ul>
                 </div>
             </article>
